356db12933795e199cdea433a73a3878dd6da694
FEG2034 <secret75310@gmail.com>
2017-12-19 21:30:46 +0800

    Update GDELT_pyspark_cloudEngine.py

--- GDELT_pyspark_cloudEngine.py
+++ GDELT_pyspark_cloudEngine.py
@@ -1,54 +1,36 @@
-from google.cloud import bigquery # <- pip install
-import networkx # <- pip install
+#python 2.7
+import networkx # <- sudo pip install networkx
 import pandas # <- pip install
 from collections import Counter # <- built-in function
 
 from pyspark import SparkConf, SparkContext
-conf = SparkConf().setMaster("local").setAppName("GDELT")
+conf = SparkConf()
 sc = SparkContext(conf = conf)
 
 from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
-spark_SQL = SparkSession(sc)
+SQLCtx = SQLContext(sc)
 
-#search_range = input(">>>Analysis period (YYYYMMDD):")
-#search_event = input(">>>Analysis event:")
-search_range = [20170303, 20170303] 
-search_event = 19 
+import sys
+search_range = [int(sys.argv[1]), int(sys.argv[2])] # as input(">>>Analysis period (YYYYMMDD):") in python
+search_event = int(sys.argv[3]) # as input(">>>Analysis event:") in python
 
-#BigQuery on python---------------------------------------------------
-def GDELT_BigQuery(search_range, search_event):
-    QUERY_W_PARAM = "SELECT Actor1CountryCode, Actor2CountryCode FROM [gdelt-bq:gdeltv2.events] \
-        WHERE EventCode LIKE '{0}%' and SQLDATE >= {1} and SQLDATE <= {2}" \
-        .format(int(search_event), search_range[0], search_range[1])
-
-    client = bigquery.Client()
-    TIMEOUT = 30  # in seconds
-    param = bigquery.ScalarQueryParameter('state', 'STRING', 'TX')
-    job_config = bigquery.QueryJobConfig()
-    job_config.query_parameters = [param]
-    query_job = client.query(QUERY_W_PARAM, job_config=job_config)
-
-    return query_job.to_dataframe()
-#---------------------------------------------------------------------
-#BigQuery on pyspark--------------------------------------------------
+#Load data from bigquery to storage--------------------------------------------------
 project = sc._jsc.hadoopConfiguration().get('fs.gs.project.id')
 bucket = sc._jsc.hadoopConfiguration().get('fs.gs.system.bucket')
-input_directory = 'gs://{}/hadoop/tmp/bigquery/pyspark_input'.format(bucket)
+input_directory = 'gs://{}/pyspark_input'.format(bucket)
 
 conf_BQ = {
     'mapred.bq.project.id': project,
     'mapred.bq.gcs.bucket': bucket,
     'mapred.bq.temp.gcs.path': input_directory,
-    'mapred.bq.input.project.id': 'gdelt-bq',
-    'mapred.bq.input.dataset.id': 'gdeltv2',
-    'mapred.bq.input.table.id': 'events'
-    'mapred.bq.input.query': 'SELECT Actor1CountryCode, Actor2CountryCode FROM [gdelt-bq:gdeltv2.events] \
-        WHERE EventCode LIKE '{0}%' and SQLDATE >= {1} and SQLDATE <= {2}'.format(int(search_event), search_range[0], search_range[1])
-}
-
-conf_BQ_test = {
-    'mapred.bq.input.table.id': 'gdelt-bq:gdeltv2.events.2017',
+    #'mapred.bq.input.project.id': 'gdelt-bq',
+    #'mapred.bq.input.dataset.id': 'gdeltv2',
+    #'mapred.bq.input.table.id': 'events',
+    'mapred.bq.input.project.id': project,
+    'mapred.bq.input.dataset.id': 'DS_GDELT_dataset',
+    'mapred.bq.input.table.id': 'results_20171206_185901',
+    #'mapred.bq.input.query': "SELECT Actor1CountryCode, Actor2CountryCode FROM `gdelt-bq.gdeltv2.events` WHERE EventCode LIKE '{0}%' and SQLDATE >= {1} and SQLDATE <= {2}".format(search_event, search_range[0], search_range[1])
 }
 
 #--Load data in from BigQuery.
@@ -56,32 +38,27 @@ table_data = sc.newAPIHadoopRDD(
     'com.google.cloud.hadoop.io.bigquery.JsonTextBigQueryInputFormat',
     'org.apache.hadoop.io.LongWritable',
     'com.google.gson.JsonObject',
-    conf=conf_BQ_test)
+    conf=conf_BQ) #Json is the problem, we need table/csv inputformat
 
 #---------------------------------------------------------------------
 
-#load data
 CountryCode = pandas.read_csv("https://www.gdeltproject.org/data/lookups/CAMEO.country.txt", sep='\t', lineterminator='\n')
 CountryCode = list(CountryCode["CODE"])
 
-#BigQuery replace here------------------------------------------------------------------------------------------------------------------------------------------------
-input = spark_SQL.read.csv("file:///Users/feg2034/GDELT_temp/GDELT - 20150218230000.export.CSV", header=True).select("Actor1CountryCode", "Actor2CountryCode") #-> return SQL.DataFrame with header
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+input = SQLCtx.createDataFrame(table_data).dropna()
+input_rdd = input.rdd.partitionBy(5)
 
-input = input.fillna("NONE")
-input_rdd = input.rdd
-
-#construct graph by NetWorkX
+#pagerank with networkx-----------------------------------------------
 G = networkx.DiGraph()
 G.add_nodes_from(CountryCode)
 
 def PR(edge):
     G_sub = networkx.create_empty_copy(G)
-    G_sub.add_edge(edge[0], edge[1])
+    G_sub.add_edges_from(list(edge))
     return list(networkx.pagerank(G_sub).items()) # can't add max_iter
 
-Pagerank = input_rdd.map(lambda x: PR(tuple(x)))
-Pagerank_result = Pagerank.reduce(lambda x,y: Counter(x)+Counter(y))
+Pagerank = input_rdd.mapPartitions(lambda x: PR(x))
+Pagerank_result = Pagerank.reduceByKey(lambda x,y: x+y).collect()
 
-ShowOff = spark_SQL.createDataFrame(Pagerank_result).toDF("Country", "Pagerank").orderBy(["Pagerank", "Country"])
-ShowOff.show(n=10)
+#ShowOff = SQLCtx.createDataFrame(Pagerank_result).toDF("Country", "Pagerank").orderBy(["Pagerank", "Country"])
+#ShowOff.show(n=10)
