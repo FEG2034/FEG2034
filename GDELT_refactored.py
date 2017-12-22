0a1f9fe86f7bf13e220240be4e3ff2595f438e40
ktapsyman <ktapsyman@gmail.com>
2017-12-23 00:36:49 +0800

    Refactoring

---
+++ GDELT_refactored.py
@@ -0,0 +1,25 @@
+#Built-in imports
+import ast 
+import sys
+import datetime
+
+#Google cloud imports
+from google.cloud import bigquery
+from google.cloud.bigquery import job
+
+#PySpark
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+
+#Global variables
+DATASET_NAME_ON_BIGQUERY = ""
+TABLE_NAME_IN_DATASET = ""
+RDD_PARTITIONS = 5
+
+def getArgs():
+	return
+
+def execuateBigQuery():
+	return
+
+def 
